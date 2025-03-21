df31c2cc734bfc18f71a8c4aea0eec7971dcea15
Havi-Dev-Py <dvszlvr@gmail.com>
2025-03-21 16:39:23 -0400

    scraping!!!

---
+++ coleta_de_dados/web_cole.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+#https://www.casadocodigo.com.br/collections/livros-de-programacao
+
+url = "https://www.casadocodigo.com.br/collections/livros-de-programacao"
+requisicao = requests.get(url)
+conteudo = BeautifulSoup(requisicao.text, features='html.parser')
+
+# print(conteudo.text.strip())
+
+
+for item in conteudo.find_all('a'):
+    titulo = item.text.strip()
+    print('Titulo: ', titulo)
