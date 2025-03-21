07c01b03f41191be239863c59d950a3e7c12735a
Havi-Dev-Py <dvszlvr@gmail.com>
2025-03-21 18:07:12 -0400

    Update pandas_.py

--- coleta_de_dados/pandas_.py
+++ coleta_de_dados/pandas_.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-#BeautifulSoup é uma classe e bs4 é o mudulo ou biblioteca
 
 resposta = requests.get("https://www.infomoney.com.br/cotacoes/b3/indice/ibovespa/historico/")
 
-sopa = BeautifulSoup(resposta.text, features='html.parser') #classes tem propriedades, a propriedade PARSE, indica que o conteúdo do site será analizado na indentação do HTML.
+sopa = BeautifulSoup(resposta.text, features='html.parser')
 
 print(sopa.prettify()[:500])
 
