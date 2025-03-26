51afa1f6b27f68e1d1c839e693a5936140cd8eaa
Havi-Dev-Py <dvszlvr@gmail.com>
2025-03-25 21:42:49 -0400

    atualizacao

--- exercicios/lib_pandas_ex001.py
+++ exercicios/lib_pandas_ex001.py
@@ -19,8 +19,30 @@ def mais_vend():
     
     return df_vendas['Produto'][id_mais_vendido]
 
+def fat_por_cat():
+    counte = eletronicos_total = moveis_total = 0
+    for itens in df_vendas.Categoria:
+        if 'Eletr' in itens:
+            eletronicos_total += df_vendas.iloc[counte, 6]
+        elif 'Móveis' in itens:
+            moveis_total += df_vendas.iloc[counte, 6]
+        counte += 1
+    return (moveis_total, eletronicos_total)
+
+def cid_mais_vendas():
+    id_mais_vendido = df_vendas['Quantidade'].idxmax()
+    
+    return df_vendas['Cidade'][id_mais_vendido]
 
 print('Faturamento total:', fatu_total())
-print(df_vendas)
-print(mais_vend())
-print(df_vendas[['Categoria', 'Produto']])
+print('Objeto mais vendido:', mais_vend())
+print(f'Cidade onde teve mais vendas: {cid_mais_vendas()}')
+print(f'Faturamento por categoria:\n',
+      f'-Eletrônicos: {fat_por_cat()[1]}\n' ,
+      f'-Móveis: {fat_por_cat()[0]}')
+
+
+#df_vendas['Data'] = pd.to_datetime(df_vendas['Data'])
+
+print('Itens mais vendidos em 02 de Fevereiro de 2024:')
+print(df_vendas[df_vendas['Data'].str.contains('2024-02-01')]['Produto'])
