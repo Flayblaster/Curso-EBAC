cc8e8a4c33f8fdc471d765603b90c250a1ab76b9
Havi-Dev-Py <dvszlvr@gmail.com>
2025-03-29 13:33:52 -0400

    coment update

--- exercicios/biblioteca_pandas/lib_pandas_ex002.py
+++ exercicios/biblioteca_pandas/lib_pandas_ex002.py
@@ -4,6 +4,13 @@ df = pd.read_csv('C:/Users/david/OneDrive/Documentos/Curso-EBAC/Curso-EBAC/exerc
 
 
 def media_salarial():
+    """
+    Cálcula a média salarial dos funcionários
+    soma - faz a soma dos salários
+    contagem - conta quantos funcionários existem
+    media - divide a soma pela contagem
+    """
+
     soma = df["Salário"].sum()
     contagem = df["ID"].count()
     media = soma/contagem
@@ -11,11 +18,26 @@ def media_salarial():
     return f'{media:.2f}'
 
 def cinco_maiores():
-    df_sorted = df.sort_values(by=['Salário'], ascending=False)
+    """
+    Encontra o cinco maiores salarios
+    df_sorted - organiza o df principal com base no salário, do maior pro menor
+    df_head - escolhe os cinco primeiros do df_sorted
+    """
 
-    return df_sorted[['Nome', 'Salário']].head(5)
+    df_sorted = df.sort_values(by=['Salário'], ascending=False)
+    df_head = df_sorted[['Nome', 'Salário']].head(5)
+
+    return df_head
 
 def qtd_por_depart():
+    """
+    Conta quantos funcionários existem por departamento
+    departs - gera uma lista com os departamentos
+    departamentos - é um df que organiza as informações em Departaemntos e Qtd_de_Pessoas
+    for - itera os departamentos
+    count - conta quantas pessoas tem naquele departamento
+    """
+    
     departs = df['Departamento'].unique()
     departamentos = pd.DataFrame(columns=['Departamentos', 'Qtd_de_Pessoas'])
     departamentos['Departamentos'] = departs
@@ -27,12 +49,27 @@ def qtd_por_depart():
     return departamentos
 
 def sal_acima_media():
+    """
+    Calcula a média salarial e mostra todos os salários acima da média
+    media_sal - média dos salários
+    acima_media - acha os salários acima da média
+    media_sorted - organiza os salários do maior pro menor
+    """
+    
     media_sal = df['Salário'].mean()
     acima_media = df.loc[df['Salário'] >= media_sal, ['Nome', 'Salário']]
-
-    return acima_media.sort_values(by=['Salário'], ascending=False)
+    media_sorted = acima_media.sort_values(by=['Salário'], ascending=False)
+    
+    return media_sorted
 
 def media_sal_depart():
+    """
+    Cálcula a média salarial por departamento
+    df_media - usa o df da funcao qtd_por_depart() para adcionar uma coluna com as medias salarial dos departamentos
+    media - acha a média dos salarios
+    Dps é criado um nova coluna chamada Media_por_Departamento
+    """
+
     df_media = qtd_por_depart()
     
     for depart in df['Departamento'].unique():
@@ -43,6 +80,11 @@ def media_sal_depart():
     return df_media[['Departamentos', 'Media_por_Departamento']]
 
 def funci_antigo():
+    """
+    acha o funcionário com mais tempo na empresa
+    antigao  - acha o menor valor da data de admissao
+    antigasso - pega a linha que pertece aquele valor de data 
+    """
     antigao = df['Data_Admissao'].min()
     antigasso = df.loc[df['Data_Admissao'] == antigao]
     return antigasso
