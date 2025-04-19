180b2363b4d1b23f4f2ba2d2831dd183e7a5884e
Havi-Dev-Py <dvszlvr@gmail.com>
2025-04-18 22:22:51 -0400

    Adição do cadastro e delete de usuários

--- exercicios/bilioteca_pymysql/ex004.py
+++ exercicios/bilioteca_pymysql/ex004.py
@@ -6,8 +6,59 @@ def conexao():
         host='localhost',
         user='root',
         password='',
-        database='cadastro',
+        database='livraria',
         cursorclass=pymysql.cursors.DictCursor
     )
+
+def cadastro_users():
+    conexao()
+    cursor = con.cursor()
+
+    while True:
+        try:
+            nome = str(input('Nome: '))
+            cpf = str(input('CPF: '))
+
+            sql = "INSERT INTO tbl_users VALUES (%s, %s)"
+            cursor.execute(sql, (cpf, nome))
+            con.commit()
+            print('Usuário cadastrado com sucesso')
+        except pymysql.Error as mysqlerror:
+            print('Erro ao cadastrar', mysqlerror)
+            con.rollback()
+        except TypeError as typerror:
+            print('Erro de digitação', typerror)
+            con.rollback()
+            continue
+        finally:
+            cursor.close()
+            con.close()
+            break
+
+def delete_user():
+    conexao()
+    cursor = con.cursor()
+
+    while True:
+        try:
+            cpf = str(input('Deletar CPF: '))
+
+            sql = "DELETE FROM tbl_users WHERE cpf = (%s)"
+            cursor.execute(sql, cpf)
+            con.commit()
+            print('Usuário deletado com sucesso')
+        except pymysql.Error as mysqlerror:
+            print('Erro ao cadastrar', mysqlerror)
+            con.rollback()
+        except TypeError as typerror:
+            print('Erro de digitação', typerror)
+            con.rollback()
+            continue
+        finally:
+            cursor.close()
+            con.close()
+            break
+
+cadastro_users()
 
 
