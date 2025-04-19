85f6c3dbde7b562c040e925f9e9b6c8c0c3211bb
Havi-Dev-Py <dvszlvr@gmail.com>
2025-04-19 18:50:06 -0400

    Atualização de funções do users

--- exercicios/bilioteca_pymysql/ex004_users.py
+++ exercicios/bilioteca_pymysql/ex004_users.py
@@ -59,3 +59,68 @@ def delete_user():
     finally:
         con.close()
         cursor.close()
+
+def atualiza_users():
+    con = conexao()
+    cursor = con.cursor()
+    sql = 0
+
+    print('1 - Nome / 2 - CPF')
+    enter = corretor_entradas('Opção de alteração: ')
+    try:
+        if '1' in enter:
+            nome_atual = corretor_entradas('Nome atual: ')
+            nome_novo = corretor_entradas('Nome novo: ')
+            sql = 'UPDATE tbl_users SET nome = (%s) WHERE nome = (%s)'
+            cursor.execute(sql, (nome_novo, nome_atual))
+        else:
+            cpf_atual = corretor_entradas('CPF atual: ')
+            cpf_novo = corretor_entradas('CPF novo: ')
+            sql = 'UPDATE tbl_users SET cpf =(%s) WHERE cpf = (%s)'
+            cursor.execute(sql, (cpf_novo, cpf_atual))
+        con.commit()
+        print('Alteração feita com sucesso')
+    except pymysql.Error as e:
+        print('Erro de update', e)
+        con.rollback()
+    finally:
+        con.close()
+        cursor.close()
+
+def listar_users():
+    con = conexao()
+    cursor = con.cursor()
+
+    try:
+        sql = "SELECT * FROM tbl_users"
+        cursor.execute(sql)
+        res = cursor.fetchall()
+
+        for coluna in res:
+            print(f'Nome: {coluna["nome"]} /// CPF: {coluna["cpf"]}')
+    finally:
+        con.close()
+        cursor.close()
+
+def listar_user():
+    con = conexao()
+    cursor = con.cursor()
+    try:
+        print('1 - Nome / 2 - CPF')
+        enter = corretor_entradas('Opção de busca: ')
+        if '1' in enter:
+            nome = corretor_entradas('Nome: ')
+            sql = 'SELECT nome, cpf FROM tbl_users WHERE nome = (%s)'
+            cursor.execute(sql, nome)
+        else:
+            cpf = corretor_entradas('CPF: ')
+            sql = 'SELECT nome, cpf FROM tbl_users WHERE cpf = (%s)'
+            cursor.execute(sql, cpf)
+            res = cursor.fetchall()
+            print(f'Nome: {res[0]['nome']} /// CPF: {res[0]['cpf']}')
+    except pymysql.Error as e:
+        print('Erro ao mostrar informações', e)
+        con.rollback()
+    finally:
+        con.close()
+        cursor.close()
