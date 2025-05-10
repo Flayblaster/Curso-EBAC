c085ba60f882d30dd80396737d65bfb69ed07c1b
Havi-Dev-Py <dvszlvr@gmail.com>
2025-05-10 13:40:16 -0400

    ex001

--- exercicios/biblioteca_matplotlib/ex001.py
+++ exercicios/biblioteca_matplotlib/ex001.py
@@ -1,1 +1,10 @@
-"estudei hojê"
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../dataframes/clima-basileia.csv')
+
+pd.set_option('display.width', None)
+
+print(df.head(20))
+print(df.info())
+print(df.describe)
