8de2752a703c2b2b0e0c3a2f7e4840196bcc954e
Havi-Dev-Py <dvszlvr@gmail.com>
2025-05-20 19:36:13 -0400

    Update intro_visualizacao.py

--- visualização_de_dados/intro_visualizacao.py
+++ visualização_de_dados/intro_visualizacao.py
@@ -5,39 +5,44 @@ import pandas as pd
 pd.set_option('display.width', None)
 
 df = pd.read_csv('../dataframes/clientes_v3.csv')
-print(df.head())
-'''
-#Histograma
-plt.hist(df['salario'])
+print(df.head().to_string())
 
-#Histograma - Parâmetros
+#Histogram
+plt.hist(df['salario'])
+#plt.show()
+
+#Histogram - Parameters
 plt.figure(figsize=(10, 6))
-plt.hist(df['salario'], bins=100, color='green', alpha=0.8)
-plt.title('Histograma - Distribuição de Salários')
-plt.xlabel('Salário')
-plt.ylabel('Frequência')
+plt.hist(df['salario'], bins=100, color='pink', alpha=1)
+plt.title('Histogram - Salaries Distribution')
+plt.xlabel('Salary')
+plt.ylabel('Frequency')
 plt.xticks(ticks=range(0, int(df['salario'].max())+2000, 2000))
 plt.grid(True)
-'''
+#plt.show()
 
-#Multiplus gráficos
+#Multy graphics
 plt.figure(figsize=(10, 6))
-plt.subplot(2, 2, 1)
-plt.scatter(df['salario'], df['anos_experiencia'], color='#ff7088', alpha=0.3, s=20)
-plt.title('Dispersão - Salário x Experiência')
-plt.xlabel('Salário')
-plt.ylabel('Experiência')
 
-plt.subplot(2, 2, 2)
-plt.bar(df['numero_filhos'], df['idade'])
-plt.title('Dispersão - Idade x Filhos')
-plt.xlabel('Idade')
-plt.ylabel('Filhos')
+#Scatter graphic
+plt.subplot(2, 2, 1) #2 linhas, 2 colunas, 2 grafico
+plt.scatter(df['salario'], df['salario'], color='pink')
+plt.title('Scatter - Salary and Salary')
+plt.xlabel('Salary')
+plt.ylabel('Salary')
 
+#Scatter graphic
+plt.subplot(1, 2, 2)
+plt.scatter(df['salario'], df['anos_experiencia'], color='pink', alpha=0.6, s=30)
+plt.title('Scatter - Salary and Tenure')
+plt.xlabel('Salary')
+plt.ylabel('Tenure')
 
+#Heat map
+corr = df[['salario', 'anos_experiencia']].corr()
 plt.subplot(2, 2, 3)
-corr = df['salario_log'] * df['idadeMinMaxScaler']
 sns.heatmap(corr, annot=True, cmap='coolwarm')
 plt.title('Correlação Salário e Idade')
 
+plt.tight_layout()
 plt.show()
