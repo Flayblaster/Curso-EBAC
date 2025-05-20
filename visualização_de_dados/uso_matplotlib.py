d07946037fe414edac67fbb291a5aa888a1a92cf
Havi-Dev-Py <dvszlvr@gmail.com>
2025-05-20 19:36:08 -0400

    exercicio de matplotlib

---
+++ visualização_de_dados/uso_matplotlib.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('../dataframes/clientes_v3.csv')
+print(df.head().to_string())
+
+#Bar graphic
+plt.figure(figsize=(10, 6))
+education_count = df['nivel_educacao'].value_counts()
+level_of_scholarity = df['nivel_educacao'].unique()
+plt.bar(x=level_of_scholarity, height=education_count, color='blue')
+plt.title('Scholarity Level')
+plt.xlabel('Education Levels')
+plt.ylabel('Amount of People')
+plt.xticks(rotation=0) #Rotacao do texto
+plt.show()
